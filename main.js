5ff19392b0e139488a5bec90abfe0bcf44ddba85
TenCreator <kk@tencreator.xyz>
2024-09-30 20:14:32 +0100

    Started on adding YTMD

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-// TO ENABLE YOUR RPC SERVER GO TO Cider > Setting > Connectivity then make sure "RPC Server" is enabled
-// TO GET YOUR API KEY GO TO Cider > Setting > Connectivity > Manage External Application Access to Cider then generator a new key and paste it in the apiKey field below
+// Please disable CORS in your browser to allow this to work.
+// Please disable security in your Cider App 2.0 client for this to work.
 
 const config = {
-    apiKey: 'ay0be3kxzi9fvvuh4lekmav9', // MUST USE IF YOU REQIRE THEM FOR YOUR RPC SERVER
-    
-    urls: {
-        host: 'http://127.0.0.1:10767',
-        baseUri: '/api/v1/playback',
-        active: '/active',
-        nowPlaying: '/now-playing',
-        isPlaying: '/is-playing',
-        volume: '/volume'
+    autoDetect: true,
+    ciderOnly: false,
+    ytmdOnly: false,
+
+    cider: {
+        urls: {
+            host: 'http://127.0.0.1:10767',
+            baseUri: '/api/v1/playback',
+            active: '/active',
+            nowPlaying: '/now-playing',
+            isPlaying: '/is-playing',
+            volume: '/volume'
+        },
     },
 
-    displayWhenNotPlaying: true,
+    ytmd: {
+        appId: 'localhost.ytmd.currently-playing',
+
+        urls: {
+            host: 'http://127.0.0.1:9863',
+            baseUri: '/api/v1',
+            requestCode: '/auth/requestcode',
+            request: '/auth/request',
+            state: '/state',
+        }
+    },
+    
+    displayWhenNotPlaying: false,
     
     songInfo: {
         display: true,
@@ -26,28 +42,49 @@ const config = {
     artwork: true
 }
 
+class Cider {
+    async makeRequest(uri) {
+        let res = false
+    
+        await fetch(config.cider.urls.host + config.cider.urls.baseUri + uri, {
+            method: 'GET',
+            headers: {},
+        })
+        .then(response => response.json())
+        .then(data => res = data)
+        .catch(error => res = false)
+    
+        return res
+    }
 
-async function makeRequest(uri) {
-    let res = false
-
-    await fetch(config.urls.host + config.urls.baseUri + uri, {
-        method: 'GET',
-        headers: {},
-    })
-    .then(response => response.json())
-    .then(data => res = data)
-    .catch(error => res = false)
-
-    return res
+    async checkActive() {
+        const isActive = await makeRequest(config.cider.urls.active)
+        const isPlaying = await makeRequest(config.cider.urls.isPlaying)
+    
+        if (isActive.status == 'ok' && isPlaying.status == 'ok' && isPlaying.is_playing) return true
+        return false
+    }
 }
 
+class YTMD {
+    appID;
+    code;
+    token;
 
-async function checkActive() {
-    const isActive = await makeRequest(config.urls.active)
-    const isPlaying = await makeRequest(config.urls.isPlaying)
+    constructor() {
+        this.appID = config.ytmd.appId
+    }
 
-    if (isActive.status == 'ok' && isPlaying.status == 'ok' && isPlaying.is_playing) return true
-    return false
+    authorize() {
+
+    }
+
+    getToken() {
+
+    }
+
+
+
 }
 
 function cutoffText(text, length) {
@@ -57,7 +94,10 @@ function cutoffText(text, length) {
 
 $('#container').hide()
 $(document).ready(async function() {
-    setInterval(async function() {
+    const cider = new Cider()
+    // const ytmd = new YTMD()
+
+    const startCider = () => setInterval(async function() {
         const isActive = await checkActive()
 
         if (isActive) {
@@ -69,7 +109,7 @@ $(document).ready(async function() {
             const songProgressBar = $('#progress-bar')
             const songArtwork = $('#artwork')
 
-            const nowPlaying = await makeRequest(config.urls.nowPlaying)
+            const nowPlaying = await makeRequest(config.cider.urls.nowPlaying)
             const [album, artist, artwork, title] = [nowPlaying.info.albumName, nowPlaying.info.artistName, nowPlaying.info.artwork, nowPlaying.info.name]
             const totalLength = nowPlaying.info.currentPlaybackTime + nowPlaying.info.remainingTime
             const progress = (nowPlaying.info.currentPlaybackTime / totalLength) * 100
@@ -80,7 +120,7 @@ $(document).ready(async function() {
             songProgressBar.css('width', progress + '%')
             
             songArtwork.attr('src', artwork.url.replace('{h}', artwork.height).replace('{w}', artwork.width))
-        } else if (config.displayWhenNotPlaying) {
+        } else if (config.cider.displayWhenNotPlaying) {
             console.log('Not Active')
             $('#container').show()
             $('#title').text('Title: Not Playing')
@@ -93,5 +133,19 @@ $(document).ready(async function() {
             $('#container').hide()
             $('#box').hide()
         }
-    }, 250)
+    }, 100)
+
+    const startYTMD = () => {
+
+    }
+
+
+    if (config.autoDetect) {
+
+    } else if (config.ciderOnly) {
+        startCider()
+    } else if (config.ytmdOnly) {
+
+    }
+
 })
