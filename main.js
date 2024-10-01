fc1bd8122d1294e21da8bc674090b1fd8575087b
TenCreator <kk@tencreator.xyz>
2024-10-01 15:48:53 +0100

    linux session 1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const config = {
     },
 
     ytmd: {
-        appId: 'localhost.ytmd.currently-playing',
+        appId: 'xyz.tencreator.ytmd.currently-playing',
 
         urls: {
             host: 'http://127.0.0.1:9863',
@@ -40,6 +40,33 @@ const config = {
     },
 
     artwork: true
+}
+
+function cutoffText(text, length) {
+    if (text.length > length) return text.substring(0, length) + '...'
+    return text
+}
+
+function updateDetails(title, artist, album, artwork, progress) {
+    title = cutoffText(title, 30)
+    artist = cutoffText(artist, 30)
+    album = cutoffText(album, 30)
+
+    $('#title').text('Title: ' + title)
+    $('#artist').text('Artist: ' + artist)
+    $('#album').text('Album: ' + album)
+
+    $('#progress-bar').css('width', progress + '%')
+
+    $('#artwork').attr('src', artwork)
+}
+
+function require(url, integrity) {
+    var script = document.createElement('script')
+    script.src = url
+    script.integrity = integrity
+    script.crossOrigin = 'anonymous'
+    document.getElementsByTagName('head')[0].appendChild(script)
 }
 
 class Cider {
@@ -68,66 +95,77 @@ class Cider {
 
 class YTMD {
     appID;
-    code;
     token;
 
     constructor() {
         this.appID = config.ytmd.appId
+        this.token = localStorage.getItem('ytmd_token')
+
+        if (!this.token) this.authorize()
+        else this.connect()
     }
 
-    authorize() {
+    async authorize() {
+        const code = await (async function() {
+            try {
+                const res = await fetch(config.ytmd.urls.host + config.ytmd.urls.baseUri + config.ytmd.urls.requestCode, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        appId: this.appID,
+                        appName: 'TenCretor\'s YTMD OBS Overlay',
+                        appVersion: '0.0.0-beta',
+                    })
+                })
+    
+                const data = await res.json()
+                return data.code
+            } catch (e) {
+                return alert('An error occurred while authorizing YTMD. Please make sure that you have YTMD installed, running and you have the Companion server enabled and try again later.')
+            }
+        }())
 
+        const token = await (async function() {
+            const res = await fetch(config.ytmd.urls.host + config.ytmd.urls.baseUri + config.ytmd.urls.request, {
+                method: 'POST',
+                body: JSON.stringify({
+                    appId: this.appID,
+                    code: code
+                })
+            })
+
+            const data = await res.json()
+            return data.token
+        }())
+
+        localStorage.setItem('ytmd_token', token)
+        this.token = token
+        // this.connect()
     }
 
-    getToken() {
-
-    }
-
-
-
-}
-
-function cutoffText(text, length) {
-    if (text.length > length) return text.substring(0, length) + '...'
-    return text
 }
 
 $('#container').hide()
 $(document).ready(async function() {
-    const cider = new Cider()
-    // const ytmd = new YTMD()
-
-    const startCider = () => setInterval(async function() {
-        const isActive = await checkActive()
+    const startCider = (cider) => setInterval(async function() {
+        const isActive = await cider.checkActive()
 
         if (isActive) {
             console.log('Active')
             $('#container').show()
-            const songTitle = $('#title')
-            const songArtist = $('#artist')
-            const songAlbum = $('#album')
-            const songProgressBar = $('#progress-bar')
-            const songArtwork = $('#artwork')
-
-            const nowPlaying = await makeRequest(config.cider.urls.nowPlaying)
+            const nowPlaying = await cider.makeRequest(config.cider.urls.nowPlaying)
+            if (!nowPlaying) return
             const [album, artist, artwork, title] = [nowPlaying.info.albumName, nowPlaying.info.artistName, nowPlaying.info.artwork, nowPlaying.info.name]
             const totalLength = nowPlaying.info.currentPlaybackTime + nowPlaying.info.remainingTime
             const progress = (nowPlaying.info.currentPlaybackTime / totalLength) * 100
 
-            songTitle.text('Title: ' + cutoffText(title, 30))
-            songArtist.text('Artist: ' + cutoffText(artist, 30))
-            songAlbum.text('Album: ' + cutoffText(album, 30))
-            songProgressBar.css('width', progress + '%')
-            
-            songArtwork.attr('src', artwork.url.replace('{h}', artwork.height).replace('{w}', artwork.width))
+            updateDetails(title, artist, album, artwork.url.replace('{h}', artwork.height).replace('{w}', artwork.width), progress)
+
         } else if (config.cider.displayWhenNotPlaying) {
             console.log('Not Active')
             $('#container').show()
-            $('#title').text('Title: Not Playing')
-            $('#artist').text('Artist: Not Playing')
-            $('#album').text('Album: Not Playing')
-            $('#progress-bar').css('width', '0%')
-            $('#artwork').attr('src', 'https://via.placeholder.com/300')
+
+            updateDetails('Not Playing', 'Not Playing', 'Not Playing', 'https://via.placeholder.com/300', 0)
+
         } else {
             console.log('Not Active, Hiding')
             $('#container').hide()
@@ -135,7 +173,7 @@ $(document).ready(async function() {
         }
     }, 100)
 
-    const startYTMD = () => {
+    const startYTMD = (ytmd) => {
 
     }
 
@@ -143,9 +181,12 @@ $(document).ready(async function() {
     if (config.autoDetect) {
 
     } else if (config.ciderOnly) {
-        startCider()
+        const cider = new Cider()
+        startCider(cider)
     } else if (config.ytmdOnly) {
-
+        require('https://cdn.socket.io/4.7.4/socket.io.min.js', 'sha384-Gr6Lu2Ajx28mzwyVR8CFkULdCU7kMlZ9UthllibdOSo6qAiN+yXNHqtgdTvFXMT4')
+        const ytmd = new YTMD()
+        startYTMD(ytmd)
     }
 
 })
